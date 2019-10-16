1548b377fafb1053620139a63b3243fff96516a4
Rizul <rizulgoyal23@gmail.com>
2019-10-15 21:25:16 -0400

    new init declaration

--- Group7_F2019_MAD3004_FP/Employee/PartTime.swift
+++ Group7_F2019_MAD3004_FP/Employee/PartTime.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
-class PartTime: Employee {
+class PartTime: Employee 
+
+{
+    
     var rate : Float?
     var hoursworked : Int?
+    
+    init(employeeID: Int, employeeName: String, employeeAge: Int, rate: Float,hoursworked: Int) {
+        super.init(employeeID: employeeID, employeeName: employeeName, employeeAge: employeeAge)
+        self.rate=rate
+        self.hoursworked=hoursworked
+    }
+    
+    
     
 }
