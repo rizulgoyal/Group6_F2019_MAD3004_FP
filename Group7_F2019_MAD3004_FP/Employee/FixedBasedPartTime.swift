cb30c37ce46fbfc34d0910bb484d1f05351f9218
Rizul <rizulgoyal23@gmail.com>
2019-10-16 20:03:44 -0400

    new class

---
+++ Group7_F2019_MAD3004_FP/Employee/FixedBasedPartTime.swift
@@ -0,0 +1,9 @@
+//
+//  FixedBasedPartTime.swift
+//  Group7_F2019_MAD3004_FP
+//
+//  Created by MacStudent on 2019-10-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
