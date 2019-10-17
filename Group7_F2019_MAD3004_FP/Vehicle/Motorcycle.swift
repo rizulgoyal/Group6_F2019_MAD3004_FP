86af43026d03c7df6cc0a7aae2638c753a872658
anmol sharma <anmolsharma433@gmail.com>
2019-10-16 20:12:02 -0400

    motorcycle class created

---
+++ Group7_F2019_MAD3004_FP/Vehicle/Motorcycle.swift
@@ -0,0 +1,14 @@
+//
+//  Motorcycle.swift
+//  Group7_F2019_MAD3004_FP
+//
+//  Created by Anmol Sharma on 2019-10-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Motorcycle: Vehicle {
+    
+    
+    
+}
