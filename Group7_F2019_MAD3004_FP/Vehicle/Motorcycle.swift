467c7e6dd03338c3ccf2edc145b772c8310352bb
anmol sharma <anmolsharma433@gmail.com>
2019-10-16 20:17:52 -0400

    super added in motorcycle

--- Group7_F2019_MAD3004_FP/Vehicle/Motorcycle.swift
+++ Group7_F2019_MAD3004_FP/Vehicle/Motorcycle.swift
@@ -14,6 +14,7 @@ class Motorcycle: Vehicle ,IPrintable {
     
     //method Iprintable
     func displayData() {
+        super.printmydata()
         print("The model is : \(model!)")
         print("Insurance is \(insurance!)")
     }
