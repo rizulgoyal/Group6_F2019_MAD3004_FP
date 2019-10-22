6841c1f8353ab89a45fed1baace48cbd305cabcf
harpreet <dandiwal357@gmail.com>
2019-10-21 21:23:40 -0400

    new extension

---
+++ Group7_F2019_MAD3004_FP/Extemnsion_Int.swift
@@ -0,0 +1,9 @@
+//
+//  Extemnsion_Int.swift
+//  Group7_F2019_MAD3004_FP
+//
+//  Created by MacStudent on 2019-10-21.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
