3647f537dce7b6cddeeae9d1975ccc60d724e7fc
Rizul <rizulgoyal23@gmail.com>
2019-10-22 20:56:08 -0400

    update function to insert vehicle object

--- Group7_F2019_MAD3004_FP/Employee/Employee.swift
+++ Group7_F2019_MAD3004_FP/Employee/Employee.swift
@@ -20,10 +20,13 @@ class Employee : Vehicle
     var vehicle = Vehicle()
     
     
+    
+    
     var birthYear : Int{
         return self.currentYear-employeeAge!
     }
 
+    
     
 //    init(employeeID:Int,employeeName:String,employeeAge:Int) {
 //        self.employeeID=employeeID
@@ -31,8 +34,151 @@ class Employee : Vehicle
 //        self.employeeAge=employeeAge
 //    }
     
+    
     func calcEarnings()
     {
+        
+    }
+    
+    func checkVehicle(vehicleDict: [String:Any])
+        
+    {
+        
+        
+        let typecheck = vehicleDict["type"] as? String
+        
+        
+        if typecheck == "Car"
+            
+        {
+            
+            var car : Car
+            
+            car = Car()
+            
+            if let make = vehicleDict["make"] as? String
+                
+            {
+                
+                car.make=make
+                
+            }
+            
+            
+            
+            if let plate = vehicleDict["plate"] as? String
+                
+            {
+                
+                car.plate=plate
+                
+            }
+            
+            
+            
+            if let type = vehicleDict["type"] as? String
+                
+            {
+                
+                car.type=type
+                
+            }
+            
+            
+            
+            
+            
+            if let model = vehicleDict["model"] as? String
+                
+            {
+                
+                car.model=model
+                
+            }
+            
+            if let insurance = vehicleDict["insurance"] as? Bool
+                
+            {
+                
+                car.insurance=insurance
+                
+            }
+            
+            car.displayData()
+            
+        }
+            
+            
+            
+            
+            
+            
+            
+        else if typecheck == "Motorcycle"
+            
+        {
+            
+            var motorcycle : Motorcycle
+            
+            motorcycle = Motorcycle()
+            
+            if let make = vehicleDict["make"] as? String
+                
+            {
+                
+                motorcycle.make=make
+                
+            }
+            
+            
+            
+            if let plate = vehicleDict["plate"] as? String
+                
+            {
+                
+                motorcycle.plate=plate
+                
+            }
+            
+            
+            
+            if let type = vehicleDict["type"] as? String
+                
+            {
+                
+                motorcycle.type=type
+                
+            }
+            
+            
+            
+            if let model = vehicleDict["model"] as? String
+                
+            {
+                
+                motorcycle.model=model
+                
+            }
+            
+            if let insurance = vehicleDict["insurance"] as? Bool
+                
+            {
+                
+                motorcycle.insurance=insurance
+                
+            }
+            
+            motorcycle.displayData()
+            
+        }
+            
+        else {
+            
+            
+            
+            print("The employee vehicle type doesnot match")
+            
+        }
         
     }
     
